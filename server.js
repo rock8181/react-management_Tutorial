5f1a645da1f20284477f3b2f829df64c4b12d183
unknown <rock8181@naver.com>
2022-05-27 00:03:51 +0900

    add rest api

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'name': 'John',
+            'email': 'rock@naver.com',
+            'phone': '010-1234-5678',            
+            'image' : 'https://randomuser.me/api/portraits/'
+        },
+        {
+            'id': 2,
+            'name': 'mary',
+            'image' : 'https://randomuser.me/api/portraits/',
+            'email': 'rock@naver.com',
+            'phone': '010-1234-5678',            
+            'image' : 'https://randomuser.me/api/portraits/'
+        }
+        ]);
 });
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
