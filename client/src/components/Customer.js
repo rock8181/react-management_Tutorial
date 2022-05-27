956863fb3feda5e73e888125f94b4cab945d6b0d
test <test@naver.com>
2022-05-28 00:28:50 +0900

    Apply Material UI

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,12 +1,43 @@
 import React from "react";
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
+import { DataGrid } from '@mui/x-data-grid';
 
+const StyledTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))`
+    & .MuiTooltip-tooltip {
+      background: green;
+    }
+  `;
 class Customer extends React.Component {
   render() {
       return (    
-      <div>
-          <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
-          <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>          
-      </div> 
+        <TableRow>
+            <TableCell>{this.props.id}</TableCell>
+            <TableCell><img src={this.props.image} alt="profile"></img></TableCell>
+            <TableCell>{this.props.name}</TableCell>
+            <TableCell>{this.props.birthday}</TableCell>
+            <TableCell>{this.props.gender}</TableCell>
+            <TableCell>
+                {this.props.job}
+                <StyledTooltip title="I am navy">
+                    <Button variant="contained" color="primary">
+                        Styled tooltip
+                    </Button>
+                </StyledTooltip>
+                <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    This is an error alert — <strong>check it out!</strong>
+                </Alert>   
+            </TableCell>            
+        </TableRow>
       )  
   }
 }
