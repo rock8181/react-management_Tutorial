a528903a51cd405bbe4e155436100fbf18fa855f
test <test@naver.com>
2022-06-02 03:09:44 +0900

    [기능] e-chart 레이다 넣기

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -24,21 +24,41 @@ class Customer extends React.Component {
 
         this.state = {
             options :{
-                        xAxis: {
-                        type: 'category',
-                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-                    },
-                        yAxis: {
-                            type: 'value'
-                    },
-                    series: [
-                        {
-                            data: [150, 230, 224, 218, 135, 147, 260],
-                            type: 'line'
-                        }
-                        ]
-                    }
-        };
+                title: {
+                  text: 'Basic Radar Chart'
+                },
+                legend: {
+                  data: ['Allocated Budget', 'Actual Spending']
+                },
+                radar: {
+                  // shape: 'circle',
+                  indicator: [
+                    { name: 'Sales', max: 6500 },
+                    { name: 'Administration', max: 16000 },
+                    { name: 'Information Technology', max: 30000 },
+                    { name: 'Customer Support', max: 38000 },
+                    { name: 'Development', max: 52000 },
+                    { name: 'Marketing', max: 25000 }
+                  ]
+                },
+                series: [
+                  {
+                    name: 'Budget vs spending',
+                    type: 'radar',
+                    data: [
+                      {
+                        value: [4200, 3000, 20000, 35000, 50000, 18000],
+                        name: 'Allocated Budget'
+                      },
+                      {
+                        value: [5000, 14000, 28000, 26000, 42000, 21000],
+                        name: 'Actual Spending'
+                      }
+                    ]
+                  }
+                ]
+              }
+                };
     }    
     render() {
         return (    
@@ -52,7 +72,7 @@ class Customer extends React.Component {
                     {this.props.job}
                     <ECharts
                     option={this.state.options}
-                    opts={{ renderer: 'svg', width: 'auto', height: '100%' }}
+                    opts={{ renderer: 'svg', width: 'auto', height: 'auto' }}
                     />
                 </TableCell>
                 <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>          
